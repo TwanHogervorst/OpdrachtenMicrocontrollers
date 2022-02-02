01294a454a99f869180bd66252c2c88f9f479146
Twan Hogervorst <twanhog@gmail.com>
2022-02-02 15:43:23 +0100

    Answer B5

--- Week 1/DeelB/B5/main.c
+++ Week 1/DeelB/B5/main.c
@@ -11,6 +11,20 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+typedef struct {
+	unsigned char port;
+	unsigned char data;
+	unsigned int delay;
+} PATTERN_STRUCT;
+
+PATTERN_STRUCT pattern[] = {
+	{ 0x00, 0x01, 100 }, { 0x01, 0x01, 100 }, { 0x02, 0x01, 100 }, { 0x03, 0x01, 100 },
+	{ 0x03, 0x03, 500 }, { 0x03, 0x07, 500 }, { 0x03, 0x0F, 500 }, { 0x03, 0x1F, 500 }, { 0x03, 0x3F, 500 }, { 0x03, 0x7F, 500 }, { 0x03, 0xFF, 500 },
+	{ 0x02, 0x81, 100 }, { 0x01, 0x81, 100 }, { 0x00, 0x81, 100 },
+	{ 0x00, 0xC1, 500 }, { 0x00, 0xE1, 500 }, { 0x00, 0xF1, 500 }, { 0x00, 0xF9, 500 }, { 0x00, 0xFD, 500 }, { 0x00, 0xFF, 500 }, { 0x00, 0xFF, 500 },
+	{ 0x00, 0xFF, 2000 }
+};
+
 void wait( int ms )
 {
 	for (int i=0; i<ms; i++) {
@@ -20,8 +34,42 @@ void wait( int ms )
 
 int main(void)
 {
+	DDRA = 0xFF;
+	DDRB = 0xFF;
+	DDRC = 0xFF;
+	DDRD = 0xFF;
+	
+	int patternLength = sizeof pattern / sizeof pattern[0];
+	
     while(1)
     {
-        //TODO:: Please write your application code 
+		PORTA = 0x00;
+		PORTB = 0x00;
+		PORTC = 0x00;
+		PORTD = 0x00;
+		
+		for (int i = 0; i < patternLength; i++) {
+			PATTERN_STRUCT pFrame = pattern[i];
+			
+			switch (pFrame.port)
+			{
+				case 0x00:
+					PORTA = pFrame.data;
+					break;
+				case 0x01:
+					PORTB = pFrame.data;
+					break;
+				case 0x02:
+					PORTC = pFrame.data;
+					break;
+				case 0x03:
+					PORTD = pFrame.data;
+					break;
+				default:
+					break;
+			}
+			
+			wait(pFrame.delay);
+		}
     }
 }
