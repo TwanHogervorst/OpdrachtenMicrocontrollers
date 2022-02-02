5a55e23b8c33487b9926ec8e71423820fa620872
Luuk Verhagen <l.verhagen5@student.avans.nl>
2022-02-02 12:59:50 +0100

    Init b3

---
+++ Week 1/DeelB/B3/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 2/2/2022 12:59:14 PM
+ *  Author: Luuk Verhagen
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
