3857479bc040ecb0b188a57503b77b6034eb7281
Twan Hogervorst <twanhog@gmail.com>
2022-02-02 11:02:09 +0100

    Added Project DeelA

---
+++ Week 1/DeelA/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 2/2/2022 11:00:31 AM
+ *  Author: twanh
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
