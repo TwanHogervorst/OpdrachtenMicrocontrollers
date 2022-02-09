abf27d44850bd71b512a298acb5ea92888610027
Twan Hogervorst <twanhog@gmail.com>
2022-02-09 16:14:34 +0100

    Add semi-answer B5

---
+++ Week 2/Week 2/B5/lcd.h
@@ -0,0 +1,30 @@
+/*
+ * lcd.h
+ *
+ * Created: 9-2-2022 12:42:27
+ *  Author: twanh
+ */ 
+
+
+#ifndef LCD_H_
+#define LCD_H_
+
+#define LCD_E_PIN 6
+#define LCD_RS_PIN 4
+//#define LCD_RST_PIN 7
+
+#define LCD_LINE_1 0
+#define LCD_LINE_2 40
+
+#ifdef LCD_LED_ON
+	#define LCD_CMD_DELAY 10
+#else
+	#define LCD_CMD_DELAY 1
+#endif
+
+void init();
+void display_text(char *str);
+void set_cursor(int position);
+
+
+#endif /* LCD_H_ */
--- Week 2/Week 2/B5/main.c
+++ Week 2/Week 2/B5/main.c
@@ -5,12 +5,42 @@
  *  Author: twanh
  */ 
 
-#include <xc.h>
+#define F_CPU 8e6
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <string.h>
+
+#include "lcd.h"
+
+void wait( int ms )
+{
+	for (int i=0; i<ms; i++) {
+		_delay_ms( 1 );
+	}
+}
 
 int main(void)
 {
-    while(1)
-    {
-        
-    }
+	
+	wait(1000);
+		
+    init();
+	
+	display_text("Welcome!");
+	
+	char* alphabet = "abcdefghijklmnopqrstuvwxyz";
+	int alphabetLength = 26; // strlen(alphabet);
+	int i = -1;
+	
+	char textToDisplay[] = { 0x00, 0x00 };
+	
+	while(1) {
+		i = (i + 1) % alphabetLength;
+		textToDisplay[0] = alphabet[i];
+		
+		set_cursor(LCD_LINE_2);
+		display_text(textToDisplay);
+		wait(500);
+	}
 }
