84d1587c377f5d430897a38fba1886c7cb7db3ad
Luuk Verhagen <l.verhagen5@student.avans.nl>
2022-02-09 10:07:45 +0100

    Added week 2 and init b2

---
+++ Week 2/Week 2/B2_250/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 2/9/2022 10:06:04 AM
+ *  Author: Luuk Verhagen
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
