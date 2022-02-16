1525048558160da48bc63e5e8d48a39631b4d43b
Twan Hogervorst <twanhog@gmail.com>
2022-02-16 10:47:27 +0100

    Added solution for week 3 and project B1

---
+++ Week 3/B1/main.c
@@ -0,0 +1,18 @@
+/*
+ * B1.c
+ *
+ * Created: 16-2-2022 10:45:35
+ * Author : twanh
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
