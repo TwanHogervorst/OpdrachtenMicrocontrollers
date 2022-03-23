c73be223745ac89233518fdf20bbeaff0f63a302
Twan Hogervorst <twanhog@gmail.com>
2022-03-23 14:42:00 +0100

    Added project for B2

---
+++ Week 5/B2/main.c
@@ -0,0 +1,18 @@
+/*
+ * B2.c
+ *
+ * Created: 23-3-2022 14:40:47
+ * Author : twanh
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
