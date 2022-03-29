27f423c2283c306bccf0e99abc7065b252da7c3d
Twan Hogervorst <twanhog@gmail.com>
2022-03-29 12:40:32 +0200

    Added project for eindopdracht

---
+++ Eindopdracht/Eindopdracht/main.c
@@ -0,0 +1,18 @@
+/*
+ * Eindopdracht.c
+ *
+ * Created: 29-3-2022 12:39:46
+ * Author : twanh
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
